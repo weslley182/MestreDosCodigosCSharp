9b243ae1c050fe4fb8426a143d7a141bb24b6730
Wesley Machado <weslley182@gmail.com>
2021-01-10 16:27:26 -0300

    Criação exercício 3

---
+++ Exerc3_Multiplos3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exerc3_Multiplos3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
