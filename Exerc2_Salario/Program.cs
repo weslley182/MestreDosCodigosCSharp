4e8e492b7d4fcd9e74b1a8a61e5de3bd21e28f31
Wesley Machado <weslley182@gmail.com>
2021-01-10 14:52:28 -0300

    Criação Exercicio 2

---
+++ Exerc2_Salario/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exerc2_Salario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
