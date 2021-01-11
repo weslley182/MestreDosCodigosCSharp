f2c693602ead2e227ab1b823d02c51d8e34824d2
Wesley Machado <weslley182@gmail.com>
2021-01-10 22:42:42 -0300

    Criação projeto 6

---
+++ Exerc6_Diferenca/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exerc6_Diferenca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
