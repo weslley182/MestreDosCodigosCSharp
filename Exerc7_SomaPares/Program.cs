53d0d281d05e70f9c75a4d3abdbb716562c35718
Wesley Machado <weslley182@gmail.com>
2021-01-10 23:23:36 -0300

    Projeto 7

---
+++ Exerc7_SomaPares/Program.cs
@@ -0,0 +1,16 @@
+﻿using Exerc7_SomaPares.Servico;
+using System;
+
+namespace Exerc7_SomaPares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculaSomaParServico calc = new CalculaSomaParServico();
+            calc.Ler4Numeros();
+            calc.RealizarCalculo();
+            Console.WriteLine(calc);
+        }
+    }
+}
---
+++ Exerc7_SomaPares/Servico/CalculaSomaParServico.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exerc7_SomaPares.Servico
+{
+    public class CalculaSomaParServico
+    {
+        List<int> lista = new List<int>();
+
+        private int _valor;
+
+        private int _valorTotal = 0;
+        private bool TestarEhPar(int numero)
+        {
+            return (numero % 2 == 0);
+        }
+
+        public void RealizarCalculo()
+        {
+            foreach (int numero in lista)
+            {
+                if (TestarEhPar(numero))
+                {
+                    _valorTotal += numero;
+                }
+            }
+        }
+
+        public void Ler4Numeros()
+        {
+            for (int i = 1; i <= 4; i++)
+            {
+                do
+                {
+                    Console.WriteLine($"Entre com o valor #{i}:");
+                } while (!int.TryParse(Console.ReadLine(), out _valor));
+
+                lista.Add(_valor);
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Soma dos valores pares é: {_valorTotal}";
+        }
+    }
+}
