9bec10d116c0cc137ee2b6d09d016a706d4c26ca
Wesley Machado <weslley182@gmail.com>
2021-01-11 10:58:37 -0300

    Exercicio 2 POO

---
+++ Exerc2_Pessoa/Entidade/Pessoa.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Exerc2_Pessoa.Entidade
+{
+    public class Pessoa
+    {
+        private readonly string _nome;
+
+        private readonly DateTime _dataNascimento;
+
+        private readonly double _altura;
+
+        private int Idade { get { return BuscarIdade(); } }
+        public Pessoa(string nome, DateTime dataNascimento, double altura)
+        {
+            _nome = nome;
+            _dataNascimento = dataNascimento;
+            _altura = altura;
+        }
+
+        private int BuscarIdade()
+        {
+            int idade = DateTime.Now.Year - _dataNascimento.Year;
+            if(DateTime.Now.DayOfYear < _dataNascimento.DayOfYear)
+            {
+                return idade - 1;
+            }
+            return idade;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Nome: ");
+            sb.Append(_nome);
+            sb.Append(", data de nascimento: ");
+            sb.Append(_dataNascimento.ToString("dd/MM/yyyy"));
+            sb.Append(", altura: ");
+            sb.Append(_altura.ToString("F2", CultureInfo.InvariantCulture));
+            sb.Append(", idade: ");
+            sb.Append(Idade.ToString());
+            return sb.ToString();
+        }
+    }
+}
---
+++ Exerc2_Pessoa/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Exerc2_Pessoa.Entidade;
+
+namespace Exerc2_Pessoa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com os dados da Pessoa:");
+
+            Console.WriteLine("Nome:");
+            string sNome = Console.ReadLine();
+
+            Console.WriteLine("Data de nascimento no formato dd/MM/yyyy:");
+            string[] sData = Console.ReadLine().Split("/");
+            
+            Console.WriteLine("Altura da pessoa separada por vírgula: (0,00)");
+            double nAltura = double.Parse(Console.ReadLine());
+
+
+            int nDia = int.Parse(sData[0]);
+            int nMes = int.Parse(sData[1]);
+            int nAno = int.Parse(sData[2]);
+
+            Pessoa pessoa = new Pessoa(sNome, new DateTime(nAno,nMes,nDia), nAltura);
+            Console.WriteLine(pessoa);
+        }
+    }
+}
