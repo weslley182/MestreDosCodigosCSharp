493c652f4a8167f1e0a800ecd9e372037bdac0e8
Wesley Machado <weslley182@gmail.com>
2021-01-10 22:51:49 -0300

    Projeto Ref e Out

--- Exerc6_Diferenca/Program.cs
+++ Exerc6_Diferenca/Program.cs
@@ -6,7 +6,32 @@ namespace Exerc6_Diferenca
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            Console.WriteLine("Crie uma aplicação, que demonstre a diferença entre REF e OUT.");            
+
+            Console.WriteLine("Variáveis nValorRef e nValorOut serão inicializadas com valor 10");
+
+            int nValorRef = 10;
+            int nValorOut;
+
+            // ref só pode ser utilizado com variáveis iniciadas
+            static void MetodoUtilizandoRef(ref int paramX)
+            {
+                paramX += 5;
+            }
+
+            // o out não precisa ser iniciado mas deve ser alterado
+            static void MetodoUtilizandoOut(out int paramX)
+            {                                
+                paramX = 5;                
+            }
+
+            MetodoUtilizandoRef(ref nValorRef);
+            
+            MetodoUtilizandoOut(out nValorOut);
+
+            Console.WriteLine($"Método utilizando ref obteve a saída {nValorRef}");
+            Console.WriteLine($"Método utilizando out obteve a saída {nValorOut}");
         }
     }
 }
