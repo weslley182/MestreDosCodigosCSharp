6e19afb8ec391c9360fe6b100f5c4a96ce30930c
Wesley Machado <weslley182@gmail.com>
2021-01-11 11:04:30 -0300

    ajustes

---
+++ Exerc3_AppBancario/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exerc3_AppBancario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
