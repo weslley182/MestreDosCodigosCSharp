27a3cceeb77034c7d6137bb7c9cea4017a92d6d0
Wesley Machado <weslley182@gmail.com>
2021-01-11 11:10:35 -0300

    projeto 4

---
+++ Exerc4_Televisao/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exerc4_Televisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
