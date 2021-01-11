a64b72988e60020d964951ee2df71e8fd721a316
Wesley Machado <weslley182@gmail.com>
2021-01-11 15:45:57 -0300

    IComparable

--- Exerc1_Demonstracao/Program.cs
+++ Exerc1_Demonstracao/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using Exerc1_Demonstracao.Entidade;
-
+using Exerc1_Demonstracao.Inferfaces;
 
 namespace Exerc1_Demonstracao
 {
@@ -8,7 +9,13 @@ namespace Exerc1_Demonstracao
     {
         static void Main(string[] args)
         {
-            //Exemplo de polimorfismo
+            ExecutarExemplosDePOOBasico();
+            ExecutarExemploIComparable();
+        }
+
+        private static void ExecutarExemplosDePOOBasico() 
+        {
+            //Abastração, polimosfismo, encapsulamento, quando usar interface
             Animal cachorro = new Cachorro();
             Console.WriteLine("Cachorro");
             cachorro.SetNome("Thor");
@@ -27,6 +34,33 @@ namespace Exerc1_Demonstracao
             PastorAlemao pastor = new PastorAlemao();
             Console.WriteLine("Pastor alemão");
             pastor.ProtegerCasa();
+        }
+
+        private static void ExecutarExemploIComparable()
+        {
+            List<Funcionario> lista = new List<Funcionario>();            
+
+            Funcionario funcionario = new Funcionario("Alex", 3000);
+            lista.Add(funcionario);
+
+            Funcionario funcionario2 = new Funcionario("Will", 2000);
+            lista.Add(funcionario2);
+
+            Funcionario funcionario3 = new Funcionario("Maria", 1000);
+            lista.Add(funcionario3);
+
+            Console.WriteLine("Apresentação da lista antes de ser reordenada.");
+            foreach(Funcionario func in lista)
+            {
+                Console.WriteLine(func);
+            }
+
+            lista.Sort();
+            Console.WriteLine("Apresentação da lista após de ser reordenada. com a a classe Funcionario comparando o Salário.");
+            foreach (Funcionario func in lista)
+            {
+                Console.WriteLine(func);
+            }
         }
 
         
